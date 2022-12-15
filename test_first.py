7b646a8807a7d1fb3aafea6f40c1a40fda229798
ErikRem <remanam@mail.ru>
2022-12-15 10:30:24 +0300

    created

---
+++ test_first.py
@@ -0,0 +1,10 @@
+from selene.support.shared import browser
+from selene import be, have
+import pytest
+
+
+browser.open('https://google.com')
+browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+print("Создал")
